d58b0ccd0f2216d6480db04f86aeebfa1e1888b7
Your Name <nhammer@gmail.com>
2017-02-09 21:05:02 -0600

    Gitignore was not ignoring properly, but now it should be

--- src/test/java/com.java.example/Experiment.java
+++ src/test/java/com.java.example/Experiment.java
@@ -1,11 +1,17 @@
 package com.java.example;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Experiment {
 
     @Test
     public void testing123() {
         System.out.println("HelloWorld");
-    }
 
+        WebDriver firefox = new FirefoxDriver();
+
+        firefox.get("www.brightpeak");
+    }
+}
