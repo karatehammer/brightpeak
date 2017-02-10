b361bfd49816e806139bb4071b777aa60ee9f0c3
Nick <nhammer@gmail.com>
2017-02-09 20:29:25 -0600

    First Commit

---
+++ src/test/java/com.java.example/Experiment.java
@@ -0,0 +1,11 @@
+package com.java.example;
+
+import org.junit.Test;
+
+public class Experiment {
+
+    @Test
+    public void testThisShit() {
+        System.out.println("What's up player");
+    }
+
